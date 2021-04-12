de50f4c5b16d403c288ae6b17c6b7d9e6938644f
Lila06 <lisa_marie_assmann@web.de>
2021-04-12 13:07:12 +0200

    Bugfix: Security update

--- CryoGridWebProject/settings.py
+++ CryoGridWebProject/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '28w)ly%970u&n+&!2u#@^dg*-r4*94^7h+da14hlxn*@sf#ac8'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'cryogrid_db',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': str(os.getenv('LOCAL_DB_KEY')),
         'HOST': 'localhost',
         'PORT': '5432',
     }
