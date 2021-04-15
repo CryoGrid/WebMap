37d9c23346ac611852d261231b57e92b39d3f403
Lila06 <lisa_marie_assmann@web.de>
2021-04-15 09:54:19 +0200

    WP008: add small example test for forcing data model

--- cgmap/models.py
+++ cgmap/models.py
@@ -30,6 +30,9 @@ class ForcingData(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     modified_at = models.DateTimeField(auto_now=True, editable=False)
 
+    def __str__(self):
+        return self.name
+
 
 class SoilType(models.Model):
     db_table = 'soil_type'
--- cgmap/tests/test_models.py
+++ cgmap/tests/test_models.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
 
+from cgmap.models import ForcingData
+
 
 # Create your tests for the models here.
+
+
 class MapModelTestCase(TestCase):
 
     def test_string_representation(self):
-        self.fail("TODO Test incomplete")
+        forcing_data = ForcingData(name='wind speed')
+        self.assertEqual(str(forcing_data), forcing_data.name)
