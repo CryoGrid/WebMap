b8f301d526e6206d446403b43a0431546b162ee1
Lisa Marie Assmann <lassmann@dmawi.de>
2021-04-23 12:18:52 +0200

    WP010: including popup interactions

--- cgmap/static/cgmap/map.js
+++ cgmap/static/cgmap/map.js
@@ -3,19 +3,21 @@ javascript file for all map related javascript code
 **/
 
 window.addEventListener("map:init", function (e) {
-                var detail = e.originalEvent ?
-                             e.originalEvent.detail : e.detail;
-                var marker = {};
-                detail.map.on('click', function(pos){
-                    lat = pos.latlng.lat;
-                    long = pos.latlng.lng;
+        var detail = e.originalEvent ?
+                     e.originalEvent.detail : e.detail;
+        var marker = {};
+        var popup = L.popup();
+        detail.map.on('click', function(pos){
+            lat = pos.latlng.lat;
+            long = pos.latlng.lng;
 
-                    console.log("You clicked the map at Lat: "+ lat+" and Long: "+long );
-                    // delete existing marker
-                    if(marker != undefined){
-                        detail.map.removeLayer(marker);
-                    };
-                    // add a new marker
-                    marker = L.marker([lat, long]).addTo(detail.map);
-                });
-            }, false);
+            console.log( "You clicked the map at Lat: "+ lat+" and Long: "+long );
+            // delete existing marker
+            if(marker != undefined){
+                detail.map.removeLayer(marker);
+            };
+            // add a new marker
+            marker = L.marker([lat, long]).addTo(detail.map).bindPopup("You clicked the map at Lat: "+ lat+" and Long: "+long).openPopup();
+            //popup.setLatLng([lat, long]).setContent( "You clicked the map at Lat: "+ lat+" and Long: "+long ).openOn(detail.map);
+        });
+    }, false);
